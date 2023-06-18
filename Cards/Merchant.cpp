e2f23c06d377019b097c72af6facbd63eab2a587
ayals99 <ayal.stein@campus.technion.ac.il>
2023-06-18 22:55:28 +0300

    Update Merchant.cpp

--- Cards/Merchant.cpp
+++ Cards/Merchant.cpp
@@ -6,43 +6,46 @@ static const int BUY_FORCE_BOOST = 2;
 static const int ZERO = 0;
 static const int INITIAL_INVALID_CHOICE = -1;
 
-int getMerchantChoiceFromUser();
+int getMerchantChoiceFromUser(Player& player);
 
-void Merchant::applyEncounter(Player& player) const{
-    printMerchantInitialMessageForInteractiveEncounter(std::cout, player.getName(), player.getCoins());
+void Merchant::applyEncounter(Player& player) const {
+    int choice = getMerchantChoiceFromUser(player);
+    int amountPayed = ZERO;
+    int action = LEAVE;
 
-    int choice = getMerchantChoiceFromUser();
-
-    if (choice == BUY_HEALTH_POTION){
-        if(player.pay(HEALTH_POTION_COST)){
-            player.heal(HEALTH_ADDITION);
-            printMerchantSummary(std::cout, player.getName(), BUY_HEALTH_POTION, HEALTH_ADDITION);
+    while (choice != LEAVE && action == LEAVE) {
+        if (choice == BUY_HEALTH_POTION) {
+            if (player.pay(HEALTH_POTION_COST)) {
+                player.heal(HEALTH_ADDITION);
+                amountPayed = HEALTH_POTION_COST;
+                action = choice;
+            }
+            else {
+                printMerchantInsufficientCoins(std::cout);
+                choice = getMerchantChoiceFromUser(player);
+            }
         }
-        else{
-            printMerchantInsufficientCoins(std::cout);
+        if (choice == BUY_FORCE_BOOST) {
+            if (player.pay(FORCE_BOOST_COST)) {
+                player.buff(FORCE_ADDITION);
+                amountPayed = FORCE_BOOST_COST;
+                action = choice;
+            }
+            else {
+                printMerchantInsufficientCoins(std::cout);
+                choice = getMerchantChoiceFromUser(player);
+            }
         }
     }
-    else if (choice == BUY_FORCE_BOOST){
-        if(player.pay(FORCE_BOOST_COST)){
-            player.buff(FORCE_ADDITION);
-            printMerchantSummary(std::cout, player.getName(), BUY_FORCE_BOOST, FORCE_ADDITION);
-        }
-        else{
-            printMerchantInsufficientCoins(std::cout);
-        }
-    }
-    else{ // choice == LEAVE
-        printMerchantSummary(std::cout, player.getName(), LEAVE, ZERO);
-    }
-
+    printMerchantSummary(std::cout, player.getName(), action, amountPayed);
 }
-
 
 bool validOption(int choice){
     return choice == LEAVE || choice == BUY_FORCE_BOOST || choice == BUY_HEALTH_POTION;
 }
 
-int getMerchantChoiceFromUser(){
+int getMerchantChoiceFromUser(Player& player){
+    printMerchantInitialMessageForInteractiveEncounter(std::cout, player.getName(), player.getCoins());
     int choice = INITIAL_INVALID_CHOICE;
     while (!validOption(choice)){
         std::cin >> choice;
