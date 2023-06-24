c8f32d771718edffa3f26ab3845e92b0fa9f2840
Berkovitz <bitay@campus.technion.ac.il>
2023-06-24 10:43:33 +0300

    small chages for readabilty

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -229,9 +229,9 @@ Player* readAndCreatePlayer()
 }
 
 std::string trim(const std::string &str) {
-    size_t first = str.find_first_not_of(' ');
+    size_t first = str.find_first_not_of(SPACE);
     if (first == std::string::npos) return "";
-    size_t last = str.find_last_not_of(' ');
+    size_t last = str.find_last_not_of(SPACE);
     return str.substr(first, last - first + 1);
 }
 
